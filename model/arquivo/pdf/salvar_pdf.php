3dd9b11fa640c45b68e6f846ac0dd952a3a941c1
DESKTOP-TR5445P\kewry <gabrielmouramarinho@gmail.com>
2024-10-07 23:31:46 -0500

    Contagem de páginas de arquivos pdf

--- model/arquivo/pdf/salvar_pdf.php
+++ model/arquivo/pdf/salvar_pdf.php
@@ -1,35 +1,43 @@
 <?php
+use \setasign\Fpdi\Fpdi;
+require_once('assets/plugins/fpdf186/fpdf.php');
+require_once('assets/plugins/fpdi2_6_1/src/autoload.php');
+
 $db                   = Conexao::getInstance();
-//$atualizacaoId        = strip_tags(@$_POST['id']);
-//$provaInstrucaoId     = @$_POST['prova_instrucao_id'];
 $arquivos             = @$_FILES;
-// $status               = 1;
 $arquivosGerais       = array();
 $error                = false;
-$mensagem             = '';
+$mensagem             = "";
+$pastaDestino         = "uploads/";
 
-foreach ($arquivos['name'] as $kArquivo => $vArquivo) {
-  $nomeReal = basename($vArquivo);
-  $retornoArquivo = salvarArquivo($arquivos['error'][$kArquivo], $nomeReal, $arquivos["tmp_name"][$kArquivo]);
-  if ($retornoArquivo['existe']) {
-    if (!$retornoArquivo['erro']) {
-      $arquivosGerais[$kArquivo]['nome']       = $nomeReal;
-      $arquivosGerais[$kArquivo]['nomeFisico'] = $retornoArquivo['arquivoNome'];
-      $arquivosGerais[$kArquivo]['tamanho']    = $arquivos['size'][$kArquivo];
-      $arquivosGerais[$kArquivo]['qtdPag']     = 10;
-    } else {
-      $arquivosGerais[$kArquivo]['erro'] = true;
-      $arquivosGerais[$kArquivo]['mensagem'] = $retornoArquivo['mensagem'];
+foreach ($arquivos as $kArquivo => $vArquivo) {
+  foreach ($vArquivo["name"] as $kArquivoSub => $vArquivoSub) {
+    $nomeReal = basename($vArquivoSub);
+    $retornoArquivo = salvarArquivo($vArquivo["error"][$kArquivoSub], $nomeReal, $vArquivo["tmp_name"][$kArquivoSub]);
+    $arquivosGerais[$kArquivoSub]["retorno"] = $retornoArquivo;
+    if ($retornoArquivo["existe"]) {
+      if (!$retornoArquivo["erro"]) {
+        $document = new Fpdi();
+        $pageCount = $document->setSourceFile($GLOBALS["pastaDestino"].$retornoArquivo["arquivoNome"]);
+        $document->close();
+        $arquivosGerais[$kArquivoSub]["nome"]       = $nomeReal;
+        $arquivosGerais[$kArquivoSub]["nomeFisico"] = $retornoArquivo["arquivoNome"];
+        $arquivosGerais[$kArquivoSub]["tamanho"]    = $vArquivo["size"][$kArquivoSub];
+        $arquivosGerais[$kArquivoSub]["qtdPag"]     = $pageCount;
+        $arquivosGerais[$kArquivoSub]["erro"]       = false;
+        $arquivosGerais[$kArquivoSub]["mensagem"]   = "sucesso";
+      } else {
+        $arquivosGerais[$kArquivoSub]["erro"]       = true;
+        $arquivosGerais[$kArquivoSub]["mensagem"]   = $retornoArquivo["mensagem"];
+      }
     }
   }
 }
-echo json_encode($arquivosGerais);
-exit();
 try {
   $db->beginTransaction();
   if (!$error) {
     foreach ($arquivosGerais as $kArquivo => $vArquivo) {
-      $stmt = $db->prepare('
+      $stmt = $db->prepare("
       INSERT INTO arquivo 
       (nome, 
         nome_fisico, 
@@ -38,85 +46,84 @@ try {
         status,  
         dt_cadastro) 
       VALUES
-      (?, ?, ?, ?, ?, NOW());');
-      $stmt->bindValue(1, $vArquivo['nome']);
-      $stmt->bindValue(2, $vArquivo['nomeFisico']);
-      $stmt->bindValue(3, $vArquivo['tamanho']);
-      $stmt->bindValue(4, $vArquivo['qtdPag']);
+      (?, ?, ?, ?, ?, NOW());");
+      $stmt->bindValue(1, $vArquivo["nome"]);
+      $stmt->bindValue(2, $vArquivo["nomeFisico"]);
+      $stmt->bindValue(3, $vArquivo["tamanho"]);
+      $stmt->bindValue(4, $vArquivo["qtdPag"]);
       $stmt->bindValue(5, 1);
       $stmt->execute();
-      $arquivosGerais[$kArquivo]['id']      = $db->lastInsertId();
-      $arquivosGerais[$kArquivo]['msg']     = 'success';
-      $arquivosGerais[$kArquivo]['retorno'] = 'Arquivo enviado com sucesso.';
+      $arquivosGerais[$kArquivo]["id"]      = $db->lastInsertId();
+      $arquivosGerais[$kArquivo]["msg"]     = "success";
+      $arquivosGerais[$kArquivo]["retorno"] = "Arquivo enviado com sucesso.";
     }
   }
   $db->commit();
-  $retorno['msg']     = 'success';
-  $retorno['retorno'] = 'Arquivos enviados com sucesso.';
+  $retorno["msg"]     = "success";
+  $retorno["retorno"] = "Arquivos enviados com sucesso.";
   echo json_encode($retorno);
   exit();
 } catch (PDOException $e) {
   $db->rollback();
-  $retorno['msg'] = 'error';
-  $retorno['retorno'] = "Erro ao tentar enviar os arquivos: +" . $e->getMessage();
+  $retorno["msg"] = "error";
+  $retorno["retorno"] = "Erro ao tentar enviar os arquivos: +" . $e->getMessage();
   echo json_encode($retorno);
   exit();
 }
 
 function salvarArquivo($error, $nome, $tmp_nome) {
-  // $tiposPermitidos = array('pdf', 'jpg', 'png', 'jpeg', 'mov'); 
-  $tiposPermitidos = array('pdf'); 
-  $pastaDestino = 'uploads/';
-  $arquivoNome = '';
+  // $tiposPermitidos = array("pdf", "jpg", "png", "jpeg", "mov"); 
+  $tiposPermitidos = array("pdf"); 
+  $arquivoNome = "";
   $erro = false;
   $existe = false;
-  $message = '';
+  $message = "";
   if ($error === UPLOAD_ERR_OK) {
     $existe = true;
     $dateNow = new DateTime();
     $arquivoTipo = strtolower(pathinfo($nome, PATHINFO_EXTENSION));
-    $arquivoNomeGravar = uniqid(rand(), true) . '-' . $dateNow->format('YmdHis') . '.' . $arquivoTipo;
-    $caminhoArquivoGravar = $pastaDestino . $arquivoNomeGravar;
+    $arquivoNomeGravar = uniqid(rand(), true) . "-" . $dateNow->format("YmdHis") . "." . $arquivoTipo;
+    $caminhoArquivoGravar = $GLOBALS["pastaDestino"] . $arquivoNomeGravar;
     if(in_array($arquivoTipo, $tiposPermitidos)){ 
       if(move_uploaded_file($tmp_nome, $caminhoArquivoGravar)){ 
         $arquivoNome = $arquivoNomeGravar;
       }else{ 
         $erro = true;
-        $message = 'Desculpe, Ocorreu um erro ao enviar seu arquivo.'; 
+        $message = "Desculpe, Ocorreu um erro ao enviar seu arquivo."; 
       } 
     }else{ 
       $erro = true;
-      $message = 'Desculpe, apenas arquivos PDF têm permissão para serem enviados.'; 
+      $message = "Desculpe, apenas arquivos PDF têm permissão para serem enviados."; 
     }
   } else {
     $erro = true;
     $existe = false;
     switch ($error) {
       case UPLOAD_ERR_INI_SIZE:
-      $message = 'O arquivo enviado excede a diretiva upload_max_filesize em php.ini';
+      $message = "O arquivo enviado excede a diretiva upload_max_filesize em php.ini";
       break;
       case UPLOAD_ERR_FORM_SIZE:
-      $message = 'O arquivo enviado excede a diretiva MAX_FILE_SIZE que foi especificada no formulário HTML';
+      $message = "O arquivo enviado excede a diretiva MAX_FILE_SIZE que foi especificada no formulário HTML";
       break;
       case UPLOAD_ERR_PARTIAL:
-      $message = 'O arquivo carregado foi carregado apenas parcialmente';
+      $message = "O arquivo carregado foi carregado apenas parcialmente";
       break;
       case UPLOAD_ERR_NO_FILE:
-      $message = 'Nenhum arquivo foi enviado';
+      $message = "Nenhum arquivo foi enviado";
       break;
       case UPLOAD_ERR_NO_TMP_DIR:
-      $message = 'Faltando uma pasta temporária';
+      $message = "Faltando uma pasta temporária";
       break;
       case UPLOAD_ERR_CANT_WRITE:
-      $message = 'Falha ao gravar arquivo no disco';
+      $message = "Falha ao gravar arquivo no disco";
       break;
       case UPLOAD_ERR_EXTENSION:
-      $message = 'Upload de arquivo interrompido por extensão';
+      $message = "Upload de arquivo interrompido por extensão";
       break;
       default:
-      $message = 'Erro de upload desconhecido';
+      $message = "Erro de upload desconhecido";
     }
   }
-  return array('arquivoNome' => $arquivoNome, 'erro' => $erro, 'existe' => $existe, 'mensagem' => $message);
+  return array("arquivoNome" => $arquivoNome, "erro" => $erro, "existe" => $existe, "mensagem" => $message);
 }
 ?>
